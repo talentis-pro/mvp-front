2f66cc64a7226a25c309117d2d9768e64c12c94e
Williancc1557 <willian.cacoelho@gmail.com>
2022-07-10 19:38:16 -0300

    refactor: use chakra for the form

--- src/components/SignInComponent.tsx
+++ src/components/SignInComponent.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -19,23 +20,23 @@ export const SignInPageComponent = () => {
 
 	return (
 		<>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<label htmlFor="email">Email</label>
-				<input type="email" {...register("email")} name="email" id="email" />
-				<label htmlFor="password">Senha</label>
-				<input
+			<FormControl onSubmit={handleSubmit(onSubmit)}>
+				<FormLabel htmlFor="email">Email</FormLabel>
+				<Input type="email" {...register("email")} name="email" id="email" />
+				<FormLabel htmlFor="password">Senha</FormLabel>
+				<Input
 					type={showPassword ? "password" : "text"}
 					{...register("password")}
 					name="password"
 					id="password"
 				/>
-				<div onClick={passwordView}>Amostrar senha</div>
-				<button type="submit">Entrar</button>
-			</form>
+				<Box onClick={passwordView}>Amostrar senha</Box>
+				<Button type="submit">Entrar</Button>
+			</FormControl>
 
-			<button onClick={() => router.push("/auth/sign-up")}>
+			<Button onClick={() => router.push("/auth/sign-up")}>
 				Página de cadastro
-			</button>
+			</Button>
 		</>
 	);
 };
