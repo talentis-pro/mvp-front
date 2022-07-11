1fd629fdb57d1244590d6d9782e1bea1a7f2776e
Willian Cavalcanti <74304047+Williancc1557@users.noreply.github.com>
2022-07-10 21:16:36 -0300

    feat: create the login page (#1)
    * feat: create the login page
    
    * refactor: use react-hook-form
    
    * refactor: use chakra for the form
    
    * refactor: clean the code
    
    * fix: change the tag Box to Button
    
    Co-authored-by: Williancc1557 <willian.cacoelho@gmail.com>

---
+++ src/components/SignInComponent.tsx
@@ -0,0 +1,44 @@
+import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export const SignInPageComponent = () => {
+	const router = useRouter();
+	const { register, handleSubmit } = useForm();
+	const [showPassword, setShowPassword] = useState(true);
+
+	const onSubmit = data => data;
+
+	return (
+		<>
+			<FormControl onSubmit={handleSubmit(onSubmit)}>
+				<FormLabel htmlFor="email">Email</FormLabel>
+				<Input type="email" {...register("email")} name="email" id="email" />
+				<FormLabel htmlFor="password">Senha</FormLabel>
+				<Input
+					type={showPassword ? "password" : "text"}
+					{...register("password")}
+					name="password"
+					id="password"
+				/>
+				<Button
+					onClick={() => {
+						if (showPassword) {
+							return setShowPassword(false);
+						}
+
+						return setShowPassword(true);
+					}}
+				>
+					Mostrar senha
+				</Button>
+				<Button type="submit">Entrar</Button>
+			</FormControl>
+
+			<Button onClick={() => router.push("/auth/sign-up")}>
+				Página de cadastro
+			</Button>
+		</>
+	);
+};
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,5 @@
+import { SignInPageComponent } from "components/SignInComponent";
+
+const SignIn = () => <SignInPageComponent />;
+
+export default SignIn;
