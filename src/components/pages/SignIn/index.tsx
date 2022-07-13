9437ad37031f7ec402c7acce297eb40757913273
End313234 <end313234@protonmail.com>
2022-07-12 21:53:49 -0300

    Add style to sign in page (#8)

--- src/components/pages/SignIn/index.tsx
+++ src/components/pages/SignIn/index.tsx
@@ -1,41 +1,130 @@
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import {
+	ArrowForwardIcon,
+	AtSignIcon,
+	ViewIcon,
+	ViewOffIcon,
+} from "@chakra-ui/icons";
+import {
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Input,
+	InputGroup,
+	InputRightElement,
+	Link,
+	Text,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFormState } from "react-hook-form";
+import type { FCWithLayout } from "types/interfaces/layout";
 
 interface Data {
 	email: string;
 	password: string;
 }
 
-export const SignInPage = () => {
-	const router = useRouter();
-	const { register, handleSubmit } = useForm<Data>();
+export const SignInPage: FCWithLayout = () => {
+	const { handleSubmit, register, control } = useForm<Data>({
+		mode: "onChange",
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+	const { isValid, errors } = useFormState({ control });
 	const [showPassword, setShowPassword] = useState(true);
 
 	const onSubmit = (data: Data) => data;
 
 	return (
 		<>
-			<FormControl onSubmit={handleSubmit(onSubmit)}>
-				<FormLabel htmlFor="email">Email</FormLabel>
-				<Input type="email" {...register("email")} name="email" id="email" />
-				<FormLabel htmlFor="password">Senha</FormLabel>
-				<Input
-					type={showPassword ? "password" : "text"}
-					{...register("password")}
-					name="password"
-					id="password"
-				/>
-				<Button onClick={() => setShowPassword(!showPassword)}>
-					Mostrar senha
-				</Button>
-				<Button type="submit">Entrar</Button>
-			</FormControl>
-
-			<Button onClick={() => router.push("/auth/sign-up")}>
-				Página de cadastro
-			</Button>
+			<Flex
+				as="section"
+				direction="column"
+				align="center"
+				justify="center"
+				w="full"
+				maxW="container.2xs"
+				minH="100vh"
+				h="full"
+				mx="auto"
+				p="6"
+			>
+				<Flex as="header" w="full" py="8">
+					<Heading as="h1">Faça login!</Heading>
+				</Flex>
+				<Flex
+					as="form"
+					direction="column"
+					align="center"
+					justify="center"
+					gap="2"
+					w="full"
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<FormControl isInvalid={Boolean(errors.email)}>
+						<FormLabel htmlFor="email">
+							<Text>E-mail</Text>
+						</FormLabel>
+						<InputGroup>
+							<Input
+								id="email"
+								type="email"
+								placeholder="exemplo@gmail.com"
+								{...register("email", {
+									required: true,
+								})}
+							/>
+							<InputRightElement>
+								<AtSignIcon />
+							</InputRightElement>
+						</InputGroup>
+					</FormControl>
+					<FormControl isInvalid={Boolean(errors.password)}>
+						<FormLabel htmlFor="password">
+							<Text>Senha</Text>
+						</FormLabel>
+						<InputGroup>
+							<Input
+								id="password"
+								type={showPassword ? "text" : "password"}
+								placeholder="Senha forte de 8+ caracteres"
+								{...register("password", {
+									required: true,
+								})}
+							/>
+							<InputRightElement>
+								<Button
+									variant="transparent"
+									onClick={() => setShowPassword(!showPassword)}
+									aria-label={showPassword ? "Esconder" : "Mostrar"}
+								>
+									{showPassword ? <ViewOffIcon /> : <ViewIcon />}
+								</Button>
+							</InputRightElement>
+						</InputGroup>
+					</FormControl>
+					<Button
+						w="full"
+						rightIcon={<ArrowForwardIcon />}
+						type="submit"
+						disabled={!isValid}
+					>
+						Faça login
+					</Button>
+				</Flex>
+				<Flex align="center" justify="flex-end" w="full" py="2">
+					<Text>
+						Não tem uma conta?{" "}
+						<NextLink href="/auth/sign-in" passHref>
+							<Link color="link">Cadastre-se</Link>
+						</NextLink>
+					</Text>
+				</Flex>
+			</Flex>
 		</>
 	);
 };
